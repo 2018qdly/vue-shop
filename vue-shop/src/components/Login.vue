fff85707d31cac92b1d11ea8ef863ce0d08b0b15
ly <lymxhh@163.com>
2021-01-06 12:29:15 +0800

    '1'

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -1,40 +1,32 @@
 <template>
   <div class="login_container">
     <div class="login_box">
+      <!-- 头像区 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="avatar" />
+      </div>
+      <!-- 登录表单 -->
       <div>
-        <!-- 登录区域 -->
         <el-form
-          label-width="0px"
-          class="login-form"
+          ref="loginFormRef"
           :model="loginForm"
           :rules="loginFormRules"
-          ref="loginFormRef"
+          label-width="60px"
+          class="login_form"
         >
-          <!-- 用户名 -->
-          <el-form-item prop="username">
-            <el-input
-              prefix-icon="iconfont icon-user"
-              v-model="loginForm.username"
-              placeholder="用户名"
-            ></el-input>
+          <el-form-item label="账号" prop="username">
+            <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
           </el-form-item>
-          <!-- 密码 -->
-          <el-form-item prop="password">
+          <el-form-item label="密码" prop="password">
             <el-input
-              prefix-icon="iconfont icon-3702mima"
               v-model="loginForm.password"
-              placeholder="密码"
               type="password"
+              prefix-icon="iconfont icon-3702mima"
             ></el-input>
           </el-form-item>
-          <!-- 按钮 -->
           <el-form-item class="btns">
-            <el-button
-            type="primary"
-            @click="login">登录</el-button>
-            <el-button
-            type="info"
-            @click="resetLoginForm">重置</el-button>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -44,80 +36,106 @@
 
 <script>
 export default {
-  data() {
+  data () {
     return {
-      //初始化表单数据
       loginForm: {
-        username: "",
-        password: "",
+        username: 'admin',
+        password: '123456'
       },
+      // 表单验证
       loginFormRules: {
         username: [
-          {
-            required: true,
-            message: "请输入登录名称",
-            trigger: "blur",
-          },
-          {
-            min: 3,
-            max: 10,
-            message: "长度在3到10个字符",
-            trigger: "blur",
-          },
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur' }
         ],
         password: [
-          {
-            required: true,
-            message: "请输入密码",
-            trigger: "blur",
-          },
-          {
-            min: 6,
-            max: 15,
-            message: "长度在6到15个字符",
-            trigger: "blur",
-          },
-        ],
-      },
-    };
+          { required: true, message: '请输入用户密码', trigger: 'blur' },
+          { min: 6, max: 18, message: '长度在 6 到 18 个字符', trigger: 'blur' }
+        ]
+      }
+    }
   },
-  methods:{
-    resetLoginForm() {
-      this.$refs.loginFormRef.resetFields();
+  methods: {
+    // 表单重置按钮
+    resetLoginForm () {
+      // console.log(this)
+      // resetFields：element-ui提供的表单方法
+      this.$refs.loginFormRef.resetFields()
     },
-    login() {
-      this.$refs.loginFormRef.validate((valid) => {
-        if(!valid)return;
-        this.$http.post('login',this.loginForm)
-      });
+    login () {
+      // 表单预验证
+      // valid：bool类型
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(valid)
+        if (!valid) return false
+        // this.$http.post('login', this.loginForm): 返回值为promise
+        // 返回值为promise，可加await简化操作 相应的也要加async
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // console.log(res)
+        if (res.meta.status !== 200) return this.$message.error('登录失败')
+        this.$message.success('登录成功')
+        // 1、将登陆成功之后的token, 保存到客户端的sessionStorage中; localStorage中是持久化的保存
+        //   1.1 项目中出现了登录之外的其他API接口，必须在登陆之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2、通过编程式导航跳转到后台主页, 路由地址为：/home
+        this.$router.push('/home')
+      })
     }
   }
-};
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+/* // lang="less" 支持less格式
+// scoped vue的指令，只在当前组件生效 */
 .login_container {
-  width: 100vw;
-  height: 100vh;
-  background-color: dimgrey;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background-color: #2b4b6b;
+  height: 100%;
 }
 .login_box {
-  position: relative;
-  width: 33vw;
-  height: 300px;
+  width: 450px;
+  height: 360px;
   background-color: #fff;
-}
-.login-form {
+  border-radius: 3px;
   position: absolute;
-  bottom: 0;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  background-color: #fff;
+
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 60px;
   width: 100%;
   padding: 0 20px;
+  box-sizing: border-box;
 }
 .btns {
   display: flex;
-  justify-content: flex-end;
+  justify-content: center;
+}
+.info {
+  font-size: 13px;
+  margin: 10px 15px;
 }
 </style>
